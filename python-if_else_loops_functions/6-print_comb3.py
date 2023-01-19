36a88b62ef2b0b1af8ef9cdf9560b342572d0365
root <5718@holbertonstudents.com>
2023-01-19 05:35:06 -0800

    Add task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for k in range(i + 1, 10):
-        if(i == 7 and k == 9):
+        if(i == 8 and k == 9):
             break
         print("{:d}{:d}, ".format(i, k), end='')
 print("89")
