b1b1dbeda5fccaa78d97a568f0459c49c6935066
root <5718@holbertonstudents.com>
2023-01-19 05:25:52 -0800

    Fix task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for k in range(i + 1, 9):
-        print("{:d}{:d} ,".format(i, k), end='')
-print("89".format(i, k))
+for i in range(0, 9):
+    for k in range(i + 1, 10):
+        if(i == 0 and k == 1):
+            print("{:d}{:d} ".format(i, k), end='')
+        else:
+            print(",{:d}{:d} ".format(i, k), end='')
