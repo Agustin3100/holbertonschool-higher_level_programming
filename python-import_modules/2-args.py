3ee3e2c1a0a19573a1649b09fa1beccd9c300029
Agustin3100 <5718@holbertonstudents.com>
2023-01-21 10:39:08 -0800

    Fix 2 task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif (len(sys.argv) == 2):
         print("1 argument:")
-        for i in range(len(sys.argv)-1):
-            print("{}: {}".format(len(sys.argv)-1, sys.argv[i + 1]))
+        for i in range(len(sys.argv)):
+            print("{}: {}".format((i), sys.argv[i]))
     elif (len(sys.argv) >= 2):
         print("{} arguments:".format(len(sys.argv)-1))
-        for i in range(1, len(sys.argv)-1):
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format((i), sys.argv[i]))
