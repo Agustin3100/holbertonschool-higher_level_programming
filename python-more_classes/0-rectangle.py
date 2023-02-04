81adc3132b6ae2ac4ae179fa72791671514757d0
Agustin3100 <5718@holbertonstudents.com>
2023-02-04 05:26:45 -0800

    Add task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+class Rectangle:
+    """Class that prints a Rectangle."""
+
+    def __init__(self):
+        """Initialize Function."""
+        Rectangle.rectangle = {}
