4616547ae038233edcb117d9bc31d7f520157365
Agustin3100 <5718@holbertonstudents.com>
2023-02-07 09:41:15 -0800

    Add task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+class BaseGeometry:
+    """Class for example."""
+
+    pass
+
+    def area(self):
+        """Class for example."""
+        raise Exception("area() is not implemented")
