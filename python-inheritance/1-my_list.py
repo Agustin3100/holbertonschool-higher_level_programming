64bb28bf8dfb0ad1ef0ec50d3d0ecb1ffbeedc53
Agustin3100 <5718@holbertonstudents.com>
2023-02-07 08:14:07 -0800

    Add task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+class MyList(list):
+    """Example class."""
+
+    def print_sorted(self):
+        """Print attributes of list in sorted order."""
+        print(sorted(self))
