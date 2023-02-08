f9c6ae5c1d492e49f6c1983214f9b9a094f57865
Agustin3100 <5718@holbertonstudents.com>
2023-02-08 04:54:48 -0800

    Add task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 """Holberton School."""
-#  BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class BaseGeometry:
-    """Class for example."""
-
-    pass
-
-    def area(self):
-        """Class for example."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Class for example."""
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
