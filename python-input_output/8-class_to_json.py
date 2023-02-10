8810db03f321103b1d9d50768b98683ad3886288
Agustin3100 <5718@holbertonstudents.com>
2023-02-10 12:13:23 -0800

    Add task 9

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,11 +2,9 @@
 """Holberton School."""
 
 
-import json
 read_file = __import__('0-read_file.py').read_file
 
 
 def class_to_json(obj):
     """My class."""
-    read_file(obj)
-    json.dumps(obj)
+    return {"name": obj.name, "number": obj.number}
