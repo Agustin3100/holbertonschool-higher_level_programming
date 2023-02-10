1e8ec583ddc8fbe822811a98a26cafd36db2b101
Agustin3100 <5718@holbertonstudents.com>
2023-02-10 04:53:10 -0800

    Add task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+def write_file(filename="", text=""):
+    """Write and return characters written."""
+    if not filename:
+        raise ValueError('Cannot convert empty string')
+    with open(filename, 'r+') as f:
+        f.read()
+        f.seek(0)
+        f.write(text)
+    return len(text)
