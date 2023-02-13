60c1a3ea4004f431dcb94b3badeb137a464ba2a3
Agustin3100 <5718@holbertonstudents.com>
2023-02-13 11:02:48 -0800

    Add task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class for example."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Init func."""
+        self.__width = width
+        self.__heigth = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Class func."""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        """Class func."""
+        return self.__heigth
+
+    @height.setter
+    def height(self, height):
+        self.__heigth = height
+
+    @property
+    def x(self):
+        """Class func."""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        """Class func."""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
