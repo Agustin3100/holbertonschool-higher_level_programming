26fd3bf00b3236cc3e837f80fde7a40ab3179deb
Agustin3100 <5718@holbertonstudents.com>
2023-03-11 06:06:03 -0800

    Remove ifs that cause an error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,88 +1,57 @@
 #!/usr/bin/python3
-"""Holberton School."""
-
-
+"""Rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Class for example."""
+    """Rectangle class that inherits from base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Init func."""
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        elif type(width) != int:
-            raise TypeError("width must be an integer")
-        elif type(x) != int:
-            raise TypeError("x must be an integer")
-        elif type(y) != int:
-            raise TypeError("y must be an integer")
-        elif height <= 0:
-            raise ValueError("height must be > 0")
-        elif width <= 0:
-            raise ValueError("width must be > 0")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = y
-            self.width = width
-            self.height = height
-            self.__x = x
+        """Class constructor."""
         super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
-        """Class func."""
+        """Getter for width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Width setter to set the value."""
         self.__width = value
 
     @property
     def height(self):
-        """Class func."""
-        return self.__heigth
+        """Getter for height."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        self.__heigth = value
+        """Height setter."""
+        self.__height = value
 
     @property
     def x(self):
-        """Class func."""
+        """Getter for x."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        """X setter."""
         self.__x = value
 
     @property
     def y(self):
-        """Class func."""
+        """Getter for x."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        """Y setter."""
         self.__y = value
 
     def area(self):
@@ -98,7 +67,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Magic method lol."""
-        return (f"[Rectangle] ({(self.id)}) {self.x}/{self.y} - {self.width}/{self.height}")
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
+                - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
         """Update func."""
