78ec3d7d28ddb7d7c77c58af12af728059a4d76a
root <5718@holbertonstudents.com>
2023-03-14 08:16:59 -0700

    List all records fix 500

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- List all records of a table
-SELECT score, name, COUNT(*)
+SELECT score, name, COUNT(*) AS ''
 FROM second_table
 WHERE name IS NOT NULL
 GROUP BY score, name ORDER BY score DESC;
