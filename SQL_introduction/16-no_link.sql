52a71973f1cca81b9b98031af103bdd75c149904
root <5718@holbertonstudents.com>
2023-03-14 07:27:01 -0700

    Fix list all records

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- List all records of a table
-SELECT score, name, COUNT(*) AS number
+SELECT score, name, COUNT(*)
 FROM second_table
-GROUP BY score, name;
+GROUP BY score, name ORDER BY score DESC;
