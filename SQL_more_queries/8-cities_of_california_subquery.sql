0e9d84757b6c0d23b1e771e242287bfd3103592f
root <5718@holbertonstudents.com>
2023-03-15 05:58:18 -0700

    Fix ciites filters

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- List all california elements
 SELECT id, name FROM cities 
 WHERE = 
-SELECT name
-FROM states
-WHERE name = 'California'
+	(SELECT name
+	FROM states
+	WHERE name = 'California');
 ORDER BY id;
