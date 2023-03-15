ddcadcd5c5902b179bf782bfd6acd159bb589dcc
root <5718@holbertonstudents.com>
2023-03-15 10:42:06 -0700

    First join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Display all cities
-SELECT COUNT(DISTINCT cities.id, cities.name, states.name) FROM cities, states
+SELECT DISTINCT cities.id, cities.name, states.name FROM cities, states
 ORDER BY cities.id;
