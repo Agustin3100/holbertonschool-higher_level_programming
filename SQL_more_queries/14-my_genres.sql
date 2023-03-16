d1d49fc8742f3b4ba12124e1bd59affbfdff8526
root <5718@holbertonstudents.com>
2023-03-16 08:23:01 -0700

    All genres of dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- Genres of Dexter
+-- All Genres of Dexter
 SELECT tv_genres.name FROM tv_genres 
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
 WHERE tv_show_genres.show_id = 8;
