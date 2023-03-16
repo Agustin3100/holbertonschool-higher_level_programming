6447af9879108829ce82e76275e00b274a265362
root <5718@holbertonstudents.com>
2023-03-16 07:39:12 -0700

    Cont shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- Count number of genres
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres 
-INNER JOIN tv_show_genres 
-ON tv_show_genres.show_id = tv_genres.id 
-GROUP BY number_of_shows; 
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+GROUP BY genre ORDER BY number_of_shows DESC;
