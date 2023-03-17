243fe1f1a34498d8d0ff4bdbf2cb4e23bb2df675
root <5718@holbertonstudents.com>
2023-03-17 07:09:30 -0700

    Fix documentation and imports

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Get all states."""
-import MySQLdb
-from sys import argv
 
-db = MySQLdb.connect(
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id")
-states = cur.fetchall()
-for state in states:
-    print(state)
-cur = cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur = cur.close()
+    db.close()
