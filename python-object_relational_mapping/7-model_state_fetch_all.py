807c66f4d201461082507ead9fcc0e36d9f26a09
root <5718@holbertonstudents.com>
2023-03-19 16:57:14 -0700

    Add more words to doc

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""All states via SQLAlchemy."""
+"""Lists all state objects listed on hbtn_0e_6_usa database."""
+
 from model_state import Base, State
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """Dont execute import."""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                         argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
