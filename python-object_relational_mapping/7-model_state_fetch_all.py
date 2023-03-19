c0bb09957b54fffe637466eef32b11b3cb5beeb2
root <5718@holbertonstudents.com>
2023-03-19 16:53:53 -0700

    Fix doc

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    """Dont execute import."""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                         argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
