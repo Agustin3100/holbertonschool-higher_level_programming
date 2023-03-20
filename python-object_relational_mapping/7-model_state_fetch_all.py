a105ebd8f62ac20796ab4adfdf9b8d7b90f05dd3
root <5718@holbertonstudents.com>
2023-03-19 17:13:37 -0700

    Add more words to doc

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all state objects that are available on hbtn_0e_6_usa database usign SQLalchemy."""
+"""Lists all state objects that are available on hbtn_0e_6_usa database usign SQLalchemy module."""
 
 from model_state import Base, State
 from sys import argv
