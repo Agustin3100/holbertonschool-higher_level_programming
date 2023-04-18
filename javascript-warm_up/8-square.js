b406791af4ca5089b55f9c1420d69ede8a4fccce
root <5718@holbertonstuendts.com>
2023-04-18 06:22:34 -0700

    Remove newline of console log

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,16 +3,15 @@
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
-  let square = "";
-  
+  let square = '';
+
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      square += "X";
+      square += 'X';
     }
-    square += "\n";
+    square += '\n';
   }
-  
-  console.log(square);
+  process.stdout.write(square);
 }
