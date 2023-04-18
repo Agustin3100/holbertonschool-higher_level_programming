204a434355dfbdbb674552ee96402d5c2965dca3
root <5718@holbertonstuendts.com>
2023-04-18 05:51:29 -0700

    Fix error with uppercase f

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const MyArray = ['C is Fun', 'Python is cool', 'JavaScript is amazing'];
+const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < MyArray.length; i++) {
   console.log(MyArray[i]);
