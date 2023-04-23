fed177fff61ed828958f633d46b743b604dfa42d
Agustin3100 <5718@holbertonstudents.com>
2023-04-23 14:52:03 -0300

    If improvement delete empty return

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-}
+    constructor (w, h) {
+      if (w && w > 0 && h && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  };
 module.exports = Rectangle;
