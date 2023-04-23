48d963d66ab01d23fd1f4aed5f3188ade8950dea
Agustin3100 <5718@holbertonstudents.com>
2023-04-23 15:27:01 -0300

    Function to print rectangle

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && w > 0 && h && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(row);
+    }
+  }
+};
