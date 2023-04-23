c833dc71fac0b96248b903609f65063eeaf94c92
Agustin3100 <5718@holbertonstudents.com>
2023-04-23 14:52:51 -0300

    Fix style

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w && w > 0 && h && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w && w > 0 && h && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  };
+  }
+}
 module.exports = Rectangle;
