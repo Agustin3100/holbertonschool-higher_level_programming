736bad6516592a198103fb61df92c0fb31cf681e
Agustin3100 <5718@holbertonstudents.com>
2023-04-24 18:16:39 -0300

    Add item to my_list ul class

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const addItem = document.querySelector('#add_item');
+const myList = document.querySelector('.my_list');
+
+addItem.addEventListener('click', function () {
+  const liElement = document.createElement('li');
+
+  liElement.innerText = 'Item';
+
+  myList.appendChild(liElement);
+});
